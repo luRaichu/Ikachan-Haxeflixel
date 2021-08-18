267793acff29d9049852af1cdc34c8046f784541
BTTRG <galaxybrix2008@gmail.com>
2021-08-18 11:48:15 -0400

    text YAY

--- source/OptionsState.hx
+++ source/OptionsState.hx
@@ -1,25 +1,32 @@
 package;
 
-import flixel.text.FlxText;
-import flixel.FlxObject;
+import flixel.util.FlxColor;
+import flixel.text.FlxBitmapText;
 import flixel.FlxG;
 import flixel.addons.display.FlxTiledSprite;
-import flixel.addons.effects.chainable.FlxEffectSprite;
-import Shaders.WiggleEffect;
-import flixel.FlxSprite;
 import flixel.addons.transition.FlxTransitionableState;
-import flixel.addons.effects.chainable.FlxWaveEffect;
+import flixel.graphics.frames.FlxBitmapFont;
 
 class OptionsState extends FlxTransitionableState
 {
-	public var wiggleShit:WiggleEffect;
 	public var bg:FlxTiledSprite;
 
     function addText(x, y, wrap, text)
     {
-        var text = new FlxText(x, y, wrap, text, 8);
-		text.setFormat("assets/data/MS Gothic.otf");
-        trace(text);
+		var font = FlxBitmapFont.fromAngelCode("assets/misc/MSX.png", "assets/misc/MSX.fnt");
+
+        var back = new FlxBitmapText(font);
+		back.text = text;
+		back.x = x + 1;
+		back.y = y + 1;
+		back.color = FlxColor.BLUE;
+		add(back);
+
+        var front = new FlxBitmapText(font);
+		front.text = text;
+		front.x = x;
+		front.y = y;
+		add(front);
     }
     override public function create():Void
     {
@@ -31,7 +38,10 @@ class OptionsState extends FlxTransitionableState
 		bg.x = -64;
 		add(bg);
 
-		var box = new FlxSprite("assets/images/Msgbox.png");
+		// JUST TESTIN SHIT HERE
+		/*var sp = new SpriteEffects('assets/images/Staff.png');
+		add(sp);
+		bg.addEffect("wave",  { id:"wave", time : 20, width : 10, height : 1 });*/
 
 		addText(0, 0, 0, "sussy balls");
 
